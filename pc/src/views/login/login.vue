faa9584deeb10d965aa6e9ff25fec51282d277dd
jrucker <jrucker@163.com>
2018-12-05 23:08:50 +0800

    [fix] UPDATE ADD ANIMATE BY THREE JS

--- pc/src/views/login/login.vue
+++ pc/src/views/login/login.vue
@@ -1,5 +1,13 @@
 <style lang="scss">
   @import "login.scss";
+
+  #indexLizi {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    bottom: 0;
+    overflow: hidden;
+  }
 </style>
 
 <template>
@@ -46,10 +54,15 @@
         </div>
       </Card>
     </div>
+
+    <div id="indexLizi">
+    </div>
   </div>
 </template>
 
 <script>
+  import THREE from "@/libs/three/three";
+
   export default {
     data() {
       return {
@@ -77,12 +90,15 @@
       loginStyle() {
         return {
           // backgroundImage: `url(${process.env.api.staticUrl}static/images/login/login_bg.jpg)`,
-          backgroundImage: `url(static/images/login/login-bg.jpg)`,
+          backgroundImage: `url(static/images/login/bg.png)`,
         }
       }
     },
     async created() {
       this.getCode();
+    },
+    mounted() {
+      this.liziInit()
     },
     methods: {
       /**
@@ -137,7 +153,95 @@
         this.$router.push({
           name: 'resetPassword'
         })
-      }
+      },
+      liziInit() {
+        var SCREEN_WIDTH = window.innerWidth;
+        var SCREEN_HEIGHT = window.innerHeight;
+        var SEPARATION = 90;
+        var AMOUNTX = 50;
+        var AMOUNTY = 50;
+        var container;
+        var particles, particle;
+        var count;
+        var camera;
+        var scene;
+        var renderer;
+        var mouseX = 0;
+        var mouseY = 0;
+        var windowHalfX = window.innerWidth / 2;
+        var windowHalfY = window.innerHeight / 2;
+        init();
+        this.interval = setInterval(loop, 1000 / 40);
+
+        function init() {
+          container = document.createElement("div");
+          container.style.position = "relative";
+          container.style.top = "200px";
+          document.getElementById("indexLizi").appendChild(container);
+          camera = new THREE.Camera(75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000);
+          camera.position.z = 1000;
+          scene = new THREE.Scene();
+          renderer = new THREE.CanvasRenderer();
+          renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+          particles = new Array();
+          var i = 0;
+          var material = new THREE.ParticleCircleMaterial(0x097bdb, 1);
+          for (var ix = 0; ix < AMOUNTX; ix++) {
+            for (var iy = 0; iy < AMOUNTY; iy++) {
+              particle = particles[i++] = new THREE.Particle(material);
+              particle.position.x = ix * SEPARATION - AMOUNTX * SEPARATION / 2;
+              particle.position.z = iy * SEPARATION - AMOUNTY * SEPARATION / 2;
+              scene.add(particle);
+            }
+          }
+          count = 0;
+          container.appendChild(renderer.domElement);
+          document.addEventListener("mousemove", onDocumentMouseMove, false);
+          document.addEventListener("touchmove", onDocumentTouchMove, false);
+        }
+
+        var mousemovetimer = null;
+
+        function onMouseMove(event) {
+          window.clearTimeout(mousemovetimer);
+          mouseX = event.clientX - windowHalfX;
+          mouseY = event.clientY - windowHalfY;
+          mousemovetimer = null;
+        }
+
+        function onDocumentMouseMove(event) {
+          if (!mousemovetimer)
+            mousemovetimer = window.setTimeout(onMouseMove, 50, event);
+        }
+
+        function onDocumentTouchMove(event) {
+          if (event.touches.length == 1) {
+            event.preventDefault();
+            mouseX = event.touches[0].pageX - windowHalfX;
+            mouseY = event.touches[0].pageY - windowHalfY;
+          }
+        }
+
+        function loop() {
+          camera.position.x += (mouseX - camera.position.x) * 0.05;
+          // camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+          camera.position.y = 364;
+          var i = 0;
+          for (var ix = 0; ix < AMOUNTX; ix++) {
+            for (var iy = 0; iy < AMOUNTY; iy++) {
+              particle = particles[i++];
+              particle.position.y =
+                Math.sin((ix + count) * 0.3) * 50 +
+                Math.sin((iy + count) * 0.5) * 50;
+              particle.scale.x = particle.scale.y =
+                (Math.sin((ix + count) * 0.3) + 1) * 2 +
+                (Math.sin((iy + count) * 0.5) + 1) * 2;
+            }
+          }
+          renderer.render(scene, camera);
+          count += 0.1;
+        }
+      },
     }
   };
 </script>
